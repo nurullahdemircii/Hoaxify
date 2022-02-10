a775e3644506c7c35486fd5f912e62b36f2e1cf3
Nurullah Demirci <nurullahdemirci061@gmail.com>
2022-02-10 03:13:03 +0300

    Initial commit.

---
+++ HoaxifyBackend/src/main/java/com/nurullahdemirci/HoaxifyBackend/HoaxifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nurullahdemirci.HoaxifyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyBackendApplication.class, args);
+	}
+
+}
